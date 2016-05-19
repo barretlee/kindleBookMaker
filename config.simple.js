21cce9b4fe9eb29bee541eeae9fe8fd58b251bad
阎王 <yanwang.lj@alibaba-inc.com>
2016-05-19 11:51:57 +0800

    modify

--- config.simple.js
+++ config.simple.js
@@ -1,7 +1,15 @@
+var moment = require('moment');
+
 module.exports = {
+  // entry: './src/KF8-Demo',
   entry: {
-    base: './simple/',
+    base: './src/KF8-Demo',
     list: []
+  },
+  /*option*/
+  output: {
+    base: './build',
+    format: '[name]-' + moment().format('YYYYMMDD')
   },
   bookInfo: {
     title: "Barret Lee's Personal Website",
@@ -10,5 +18,13 @@ module.exports = {
     copyright: "Barret Lee",
     publisher: "",
     coverImage: 'coverImage.png'
+  },
+  /*option*/
+  push2kindle: {
+    nodemailer: {
+      email: 'barret.china@gmail.com',
+      password: 'your-email-password',
+      kindle: 'barretlee.com@kindle.cn'
+    }
   }
 };
