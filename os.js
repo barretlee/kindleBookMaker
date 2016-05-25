4b8231f35e037920c33ead2eca26c2680430735a
Hillwah <curvycode@gmail.com>
2016-05-25 14:40:14 +0800

    delete

--- os.js
+++
@@ -1,9 +0,0 @@
-var os   = require('os');
-var fs   = require('fs');
-var rimraf = require('rimraf');
-var path = require('path');
-
-var type = os.type();
-console.log(type);
-var entry = 'src/demo';
-console.log(path.join(entry, 'toc.*'));
